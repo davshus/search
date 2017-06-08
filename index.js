05dbeb1300b4908a2b5b07c6969f71437bf0dfc5
David Shustin <david.shustin@gmail.com>
2017-06-07 20:02:46 -0400

    Restructure and add search.clear()

--- index.js
+++ index.js
@@ -1,63 +1,26 @@
 'use strict';
 const readline = require('readline');
 const chalk = require('chalk');
+const search = require('./search');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 rl.setPrompt(chalk.magenta.dim('Enter a search item: '));
 var getQuery = false;
-var unique = (input) => input.toLowerCase().split("").reduce((a, b) => a + (a.indexOf(b) == -1 ? b : ""), "");
-var funcs = {
-  _list: [],
-  register: function(input) {
-    this._list.push(input);
-  },
-  search: function(regex) {
-    let direct = regex;
-    let loose = regex.toLowerCase()
-      .split("")
-      .reduce((a, b) => a == '' ? '(' + b + ')' : a + '[^' + b + ']*(' + b + ')', '') + '.*';
-    let directMatches = [], looseMatches = [];
-    for (let i = 0; i < this._list.length; i++) {
-      let directMatch = this._list[i].match(direct), looseMatch = this._list[i].match(loose);
-      if (directMatch) {
-        directMatches.push(this._list[i].substring(0, directMatch.index)
-          + chalk.green.bold(this._list[i].substring(directMatch.index, directMatch.index + directMatch[0].length))
-          + this._list[i].substring(directMatch.index + directMatch[0].length));
-      } else if (looseMatch) {
-        if (!looseMatch) continue;
-        let looseMatchLetterInd = 1, matchBuilder = '';
-        for (let j = looseMatch.index; j < this._list[i].length; j++) {
-          if (this._list[i][j] == looseMatch[looseMatchLetterInd]) {
-            matchBuilder += chalk.green.bold(this._list[i][j]);
-            looseMatchLetterInd++;
-            if (looseMatchLetterInd == looseMatch.length) {
-              matchBuilder += this._list[i].substring(j + 1);
-              break;
-            }
-          } else matchBuilder += this._list[i][j];
-        }
-        looseMatches.push(matchBuilder);
-      }
-    }
-    let returnMatches = directMatches;
-    returnMatches.push.apply(returnMatches, looseMatches);
-    return returnMatches;
-  }
-};
 console.log(chalk.blue('Enter \'\\\' to search.'));
 rl.prompt();
 rl.on('line', (input) => {
   if (getQuery) {
-    funcs.search(input).forEach((i) => console.log(chalk.blue.bold('RESULT: '), i));
-    rl.setPrompt(chalk.magenta.dim('Enter a search item: '))
+    search.query(input).forEach((i) => console.log(chalk.blue.bold('RESULT:'), i));
+    rl.setPrompt(chalk.magenta.dim('Enter a search item: '));
     getQuery = false;
+    search.clear();
   } else if (input == '\\') {
     getQuery = true;
     rl.setPrompt(chalk.magenta.dim('Enter a search query: '));
   } else {
-    funcs.register(input);
+    search.register(input);
   }
   rl.prompt();
 });
